6f697e40a1b1e3cfc28c72402a7765c31b459eed
Andrei Ivanchihin <andrei.ivanchihin@firstlinesoftware.com>
2023-10-04 10:47:45 +0200

    Added parsing from xml definitions from design mode

--- PropertiesGenerator/Program.cs
+++ PropertiesGenerator/Program.cs
@@ -1,51 +1,100 @@
-﻿namespace PropertiesGenerator;
+﻿using System.Text;
+using System.Xml;
+
+namespace PropertiesGenerator;
 
 class Program
 {
-    private static readonly string inputFilePath = "inputDefinitions.txt";
-    private static readonly string outputDirectoryPath = "outputProperties";
+	private static readonly string inputFilePath = "inputDefinitions.txt";
+	private static readonly string xmlInputFilePath = "inputDefinitions.xml";
+	private static readonly string outputDirectoryPath = "outputProperties";
 
-    static void Main()
-    {
-        //var tmp = Directory.GetCurrentDirectory();
-        Console.WriteLine("1 - Definitions to DTO C# properties (from buffer into buffer)");
-        Console.WriteLine("2 - Definitions to Details C# properties (from buffer into buffer)");
-        Console.WriteLine("3 - Definitions to DetailsData C# properties (from buffer into buffer)");
-        Console.WriteLine("4 - Map Definitions (from buffer into buffer)");
-        Console.WriteLine($"5 - All above (from {inputFilePath} file to {outputDirectoryPath} folder)");
-        int choice =  int.Parse(Console.ReadLine());
-        if (choice < 5)
-        {
-            Clipboard.WriteToClipboard(
-                PropertyParser.ParseProperties(
-                    Clipboard.ReadFromClipboard(), choice));
-        }
-        else
-        {
-            if(!File.Exists(inputFilePath))
-                throw new FileNotFoundException($"File {inputFilePath} not found");
-            if(!Directory.Exists(outputDirectoryPath))
-                Directory.CreateDirectory(outputDirectoryPath);
-            var dataFromFile = string.Join("/r/n", File.ReadAllText(inputFilePath));
-            for (int i = 1; i < 5; i++)
-            {
-                string parsedData = PropertyParser.ParseProperties(dataFromFile, i);
-                switch (i)
-                {
-                    case 1:                       
-                        File.WriteAllText(Path.Combine(outputDirectoryPath, "DtoProperties.cs"), parsedData);
-                        break;
-                    case 2:
-                        File.WriteAllText(Path.Combine(outputDirectoryPath, "DetailsProperties.cs"), parsedData);
-                        break;
-                    case 3:
-                        File.WriteAllText(Path.Combine(outputDirectoryPath, "DetailsDataProperties.cs"), parsedData);
-                        break;
-                    case 4:
-                        File.WriteAllText(Path.Combine(outputDirectoryPath, "Map.cs"), parsedData);
-                        break;
-                }
-            }
-        }
-    }
+	static void Main()
+	{
+		//var tmp = Directory.GetCurrentDirectory();
+		Console.WriteLine("1 - Definitions to DTO C# properties (from buffer into buffer)");
+		Console.WriteLine("2 - Definitions to Details C# properties (from buffer into buffer)");
+		Console.WriteLine("3 - Definitions to DetailsData C# properties (from buffer into buffer)");
+		Console.WriteLine("4 - Map Definitions (from buffer into buffer)");
+		Console.WriteLine($"5 - All above (from {inputFilePath} file to {outputDirectoryPath} folder)");
+		Console.WriteLine("4 - Map xml definitions (from inputDefinitions.xml)");
+		int choice = int.Parse(Console.ReadLine());
+		if (choice < 5)
+		{
+			Clipboard.WriteToClipboard(
+				PropertyParser.ParseProperties(
+					Clipboard.ReadFromClipboard(), choice));
+		}
+		else if (choice == 6)
+		{
+			string dataFromFile = "";
+
+			// Load the XML document from the file
+			XmlDocument xmlDoc = new XmlDocument();
+			xmlDoc.Load(xmlInputFilePath);
+
+			StringBuilder extractedData = new StringBuilder();
+
+			// Extract name and type attributes as columns
+			XmlNodeList fieldNodes = xmlDoc.SelectNodes("//field");
+			foreach (XmlNode fieldNode in fieldNodes)
+			{
+				string name = fieldNode.Attributes["name"].Value;
+				string type = fieldNode.Attributes["type"].Value;
+				string? limit = fieldNode.Attributes["limiting_length"]?.Value;
+				extractedData.AppendLine($"{name} {type} {limit}");
+			}
+
+			if (!Directory.Exists(outputDirectoryPath))
+				Directory.CreateDirectory(outputDirectoryPath);
+			dataFromFile = extractedData.ToString();
+
+			for (int i = 1; i < 5; i++)
+			{
+				string parsedData = PropertyParser.ParseProperties(dataFromFile, i);
+				switch (i)
+				{
+					case 1:
+						File.WriteAllText(Path.Combine(outputDirectoryPath, "DtoProperties.cs"), parsedData);
+						break;
+					case 2:
+						File.WriteAllText(Path.Combine(outputDirectoryPath, "DetailsProperties.cs"), parsedData);
+						break;
+					case 3:
+						File.WriteAllText(Path.Combine(outputDirectoryPath, "DetailsDataProperties.cs"), parsedData);
+						break;
+					case 4:
+						File.WriteAllText(Path.Combine(outputDirectoryPath, "Map.cs"), parsedData);
+						break;
+				}
+			}
+		}
+		else
+		{
+			if (!File.Exists(inputFilePath))
+				throw new FileNotFoundException($"File {inputFilePath} not found");
+			if (!Directory.Exists(outputDirectoryPath))
+				Directory.CreateDirectory(outputDirectoryPath);
+			var dataFromFile = string.Join("/r/n", File.ReadAllText(inputFilePath));
+			for (int i = 1; i < 5; i++)
+			{
+				string parsedData = PropertyParser.ParseProperties(dataFromFile, i);
+				switch (i)
+				{
+					case 1:
+						File.WriteAllText(Path.Combine(outputDirectoryPath, "DtoProperties.cs"), parsedData);
+						break;
+					case 2:
+						File.WriteAllText(Path.Combine(outputDirectoryPath, "DetailsProperties.cs"), parsedData);
+						break;
+					case 3:
+						File.WriteAllText(Path.Combine(outputDirectoryPath, "DetailsDataProperties.cs"), parsedData);
+						break;
+					case 4:
+						File.WriteAllText(Path.Combine(outputDirectoryPath, "Map.cs"), parsedData);
+						break;
+				}
+			}
+		}
+	}
 }
--- PropertiesGenerator/PropertyParser.cs
+++ PropertiesGenerator/PropertyParser.cs
@@ -5,266 +5,272 @@ namespace PropertiesGenerator;
 
 public static class PropertyParser
 {
-    public static string ParseProperties(string input, int choice)
-    {
-        char[] delimiters = { '\r', '\n' };
-        string[] strings = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
-        var output = new StringBuilder();
-        foreach (var str in strings)
-        {
-            try
-            {
-                output.Append(ParseProperty(str, choice));
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Error with {str} : {e.Message}");
-            }
-        }
+	public static string ParseProperties(string input, int choice)
+	{
+		char[] delimiters = { '\r', '\n' };
+		string[] strings = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+		var output = new StringBuilder();
+		foreach (var str in strings)
+		{
+			try
+			{
+				output.Append(ParseProperty(str, choice));
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Error with {str} : {e.Message}");
+			}
+		}
 
-        var result = output.ToString().Trim();
-        return choice == 4 ?
-            result.Remove(result.Length - 1) :
-            result;
-    }
+		var result = output.ToString().Trim();
+		return choice == 4 ?
+			result.Remove(result.Length - 1) :
+			result;
+	}
 
-    public static string ParseProperty(string input, int choice)
-    {
-        var variable = GetDataFromInput(input, out string? type);
-        return choice switch
-        {
-            1 => GetDtoProperty(variable, type),
-            2 => GetDetailsProperty(variable, type),
-            3 => GetDetailsDataProperty(variable, type),
-            4 => GetMapDefinition(variable),
-            _ => throw new Exception("Invalid choice")
-        };
-    }
+	public static string ParseProperty(string input, int choice)
+	{
+		var variable = GetDataFromInput(input, out string? type);
+		return choice switch
+		{
+			1 => GetDtoProperty(variable, type),
+			2 => GetDetailsProperty(variable, type),
+			3 => GetDetailsDataProperty(variable, type),
+			4 => GetMapDefinition(variable),
+			_ => throw new Exception("Invalid choice")
+		};
+	}
 
-    private static string GetDtoProperty(string variable, string? type)
-    {
-        var (pascalCaseComment, propertyType, pascalCaseVariable) = 
-            GetParsedFromVariableAndType(variable, type);
+	private static string GetDtoProperty(string variable, string? type)
+	{
+		var (pascalCaseComment, propertyType, pascalCaseVariable) =
+			GetParsedFromVariableAndType(variable, type);
 
-        return $@"/// <summary>
+		return $@"/// <summary>
 /// {pascalCaseComment}.
 /// </summary>
 public {propertyType} {pascalCaseVariable} {{ get; set; }}{GetDefaultString(propertyType)}
 
 ";
-    }
+	}
 
-    private static string GetDetailsProperty(string variable, string? type)
-    {
-        var (_, propertyType, pascalCaseVariable) =
-            GetParsedFromVariableAndType(variable, type);
+	private static string GetDetailsProperty(string variable, string? type)
+	{
+		var (_, propertyType, pascalCaseVariable) =
+			GetParsedFromVariableAndType(variable, type);
 
-        return $@"public {propertyType} {pascalCaseVariable} {{ get; set; }}{GetDefaultString(propertyType)}
+		return $@"public {propertyType} {pascalCaseVariable} {{ get; set; }}{GetDefaultString(propertyType)}
 
 ";
-    }
+	}
 
-    private static string GetDetailsDataProperty(string variable, string? type)
-    {
-        var (_, propertyType, pascalCaseVariable) =
-            GetParsedFromVariableAndType(variable, type);
-        return $@"[JsonProperty(""{variable}"")]
+	private static string GetDetailsDataProperty(string variable, string? type)
+	{
+		var (_, propertyType, pascalCaseVariable) =
+			GetParsedFromVariableAndType(variable, type);
+		return $@"[JsonProperty(""{variable}"")]
 public {propertyType} {pascalCaseVariable} {{ get; set; }}{GetDefaultString(propertyType)}
 
 ";
-    }
+	}
 
-    private static string GetMapDefinition(string variable)
-    {
-         var (_, _, pascalCaseVariable) =
-            GetParsedFromVariableAndType(variable);
+	private static string GetMapDefinition(string variable)
+	{
+		var (_, _, pascalCaseVariable) =
+		   GetParsedFromVariableAndType(variable);
 
-        return $@"{pascalCaseVariable} = source.{pascalCaseVariable},
+		return $@"{pascalCaseVariable} = source.{pascalCaseVariable},
 ";
-    }
+	}
 
-    private static string GetDefaultString(string?  propertyType)
-    {
-        return propertyType == "string" ? " = string.Empty;" : string.Empty;
-    }
+	private static string GetDefaultString(string? propertyType)
+	{
+		return propertyType == "string" ? " = string.Empty;" : string.Empty;
+	}
 
-    private static (string, string, string) GetParsedFromVariableAndType(string variable, string? type = null)
-    {
-        var pascalCaseComment = GetComment(variable);
-        string propertyType = GetType(type, pascalCaseComment);
-        string pascalCaseVariable = GetVariable(pascalCaseComment);
+	private static (string, string, string) GetParsedFromVariableAndType(string variable, string? type = null)
+	{
+		var pascalCaseComment = GetComment(variable);
+		string propertyType = GetType(type, pascalCaseComment);
+		string pascalCaseVariable = GetVariable(pascalCaseComment);
 
-        return (pascalCaseComment, propertyType, pascalCaseVariable);
-    }
-    
-    private static string GetDataFromInput(string input, out string? type)
-    {
-        int closingBracketIndex = input.IndexOf(']');
-        string variable;
-        try
-        {
-            if (closingBracketIndex != -1)
-            {
-                int separatorIndex = input.IndexOfAny(new[] { ' ', '-' }, closingBracketIndex + 1);
-                if (separatorIndex == -1)
-                    separatorIndex = input.Length;
+		return (pascalCaseComment, propertyType, pascalCaseVariable);
+	}
 
-                variable = input.Substring(closingBracketIndex + 1, separatorIndex - closingBracketIndex - 1).Trim();
-                type = null;
+	private static string GetDataFromInput(string input, out string? type)
+	{
+		int closingBracketIndex = input.IndexOf(']');
+		string variable;
+		try
+		{
+			if (closingBracketIndex != -1)
+			{
+				int separatorIndex = input.IndexOfAny(new[] { ' ', '-' }, closingBracketIndex + 1);
+				if (separatorIndex == -1)
+					separatorIndex = input.Length;
 
-                int openingParenIndex = input.LastIndexOf('(');
-                int closingParenIndex = input.LastIndexOf(')');
+				variable = input.Substring(closingBracketIndex + 1, separatorIndex - closingBracketIndex - 1).Trim();
+				type = null;
 
-                if (openingParenIndex != -1 && closingParenIndex != -1)
-                {
-                    type = input.Substring(openingParenIndex + 1, closingParenIndex - openingParenIndex - 1).Trim();
-                }
-            }
-            else
-            {
-                char[] whitespaceChars = { ' ', '\t', '\n', '\r', '\f', '\v' };
+				int openingParenIndex = input.LastIndexOf('(');
+				int closingParenIndex = input.LastIndexOf(')');
 
-                string[] words = input.Split(whitespaceChars, StringSplitOptions.RemoveEmptyEntries);
-                type = words[1];
-                variable = GetParsedWord(words[0], out _);
-            }
-        }
-        catch
-        {
-            throw new Exception($"Invalid input format {input}");
-        }
+				if (openingParenIndex != -1 && closingParenIndex != -1)
+				{
+					type = input.Substring(openingParenIndex + 1, closingParenIndex - openingParenIndex - 1).Trim();
+				}
+			}
+			else
+			{
+				char[] whitespaceChars = { ' ', '\t', '\n', '\r', '\f', '\v' };
 
-        return variable;
-    }
+				string[] words = input.Split(whitespaceChars, StringSplitOptions.RemoveEmptyEntries);
+				type = words[1];
+				variable = GetParsedWord(words[0], out _);
+			}
+		}
+		catch
+		{
+			throw new Exception($"Invalid input format {input}");
+		}
 
-    private static string GetComment(string input)
-    {
-        var result = new StringBuilder();
-        var word = new StringBuilder();
-        bool capitalizedPrev = true;
-        bool makeNextUppercase = false;
+		return variable;
+	}
 
-        input = Regex.Replace(input, @"\[.*?\]", "");
+	private static string GetComment(string input)
+	{
+		var result = new StringBuilder();
+		var word = new StringBuilder();
+		bool capitalizedPrev = true;
+		bool makeNextUppercase = false;
 
-        foreach (char currentChar in input)
-        {
-            if (currentChar is '-' or '_')
-            {
-                makeNextUppercase = true;
-                continue;
-            }
+		input = Regex.Replace(input, @"\[.*?\]", "");
 
-            var tmpCurrentChar = currentChar;
-            if (makeNextUppercase)
-            {
-                makeNextUppercase = false;
-                tmpCurrentChar = char.ToUpper(tmpCurrentChar);
-            }
+		foreach (char currentChar in input)
+		{
+			if (currentChar is '-' or '_')
+			{
+				makeNextUppercase = true;
+				continue;
+			}
 
-            if (!capitalizedPrev && char.IsUpper(tmpCurrentChar) ||
-                (char.IsLower(tmpCurrentChar) && !word.ToString().Skip(1).Any(char.IsLower) && word.Length > 1))
-            {
-                var parsedWord = GetParsedWord(word.ToString(), out bool areAllCapital);
-                if (areAllCapital)
-                {
-                    result.Append(parsedWord[..^1]);
-                    word = new StringBuilder(parsedWord[^1] + char.ToLower(tmpCurrentChar).ToString());
-                }
-                else
-                {
-                    result.Append(parsedWord);
-                    word = new StringBuilder(char.ToLower(tmpCurrentChar).ToString());
-                }
-                
-                result.Append(' ');
-                
-            }
-            else
-            {
-                word.Append(tmpCurrentChar);
-            }
+			var tmpCurrentChar = currentChar;
+			if (makeNextUppercase)
+			{
+				makeNextUppercase = false;
+				tmpCurrentChar = char.ToUpper(tmpCurrentChar);
+			}
 
-            capitalizedPrev = char.IsUpper(tmpCurrentChar);
-        }
+			if (!capitalizedPrev && char.IsUpper(tmpCurrentChar) ||
+				(char.IsLower(tmpCurrentChar) && !word.ToString().Skip(1).Any(char.IsLower) && word.Length > 1))
+			{
+				var parsedWord = GetParsedWord(word.ToString(), out bool areAllCapital);
+				if (areAllCapital)
+				{
+					result.Append(parsedWord[..^1]);
+					word = new StringBuilder(parsedWord[^1] + char.ToLower(tmpCurrentChar).ToString());
+				}
+				else
+				{
+					result.Append(parsedWord);
+					word = new StringBuilder(char.ToLower(tmpCurrentChar).ToString());
+				}
 
-        result.Append(GetParsedWord(word.ToString(), out _));
-        return result.ToString();
-    }
+				result.Append(' ');
 
-    private static string GetVariable(string pascalCaseComment)
-    {
-        var result = new StringBuilder();
-        var words = pascalCaseComment.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+			}
+			else
+			{
+				word.Append(tmpCurrentChar);
+			}
 
-        foreach (var word in words)
-        {
-            result.Append(word[0].ToString().ToUpper() + word[1..]);
-        }
+			capitalizedPrev = char.IsUpper(tmpCurrentChar);
+		}
 
-        return result.ToString();
-    }
+		result.Append(GetParsedWord(word.ToString(), out _));
+		return result.ToString();
+	}
 
-    private static string GetParsedWord(string input, out bool areAllCapital)
-    {
-        string? result;
-        var endingNumbers = input[^input.Reverse().TakeWhile(char.IsDigit).Count()..];
-        if (string.IsNullOrWhiteSpace(endingNumbers))
-        {
-            Abbreviations.TryGetValue(input.ToLower(), out result);
-            result ??= input;
-        }
-        else
-        {
-            Abbreviations.TryGetValue(input[..^endingNumbers.Length].ToLower(), out result);
-            result = string.IsNullOrEmpty(result) ? input : result + endingNumbers;
-        }
+	private static string GetVariable(string pascalCaseComment)
+	{
+		var result = new StringBuilder();
+		var words = pascalCaseComment.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-        
-        if (result.Skip(1).Any(char.IsLower) || result.Length is 1)
-        {
-            areAllCapital = false; 
-        }
-        else
-        {
-            result = result.ToUpper();
-            areAllCapital = true;
-        }
+		foreach (var word in words)
+		{
+			result.Append(word[0].ToString().ToUpper() + word[1..]);
+		}
 
-        return  result;
-    }
+		return result.ToString();
+	}
 
-    private static string GetType(string? comment, string variable)
-    {
-        if (comment != null)
-        {
-            return comment.ToLower() switch
-            {
-                "alpha" => "string",
-                "text" => "string",
-                "real" => "float",
-                "integer" => "int",
-                "long" => "long",
-                "boolean" => "bool",
-                _ => "string"
-            };
-        }
+	private static string GetParsedWord(string input, out bool areAllCapital)
+	{
+		string? result;
+		var endingNumbers = input[^input.Reverse().TakeWhile(char.IsDigit).Count()..];
+		if (string.IsNullOrWhiteSpace(endingNumbers))
+		{
+			Abbreviations.TryGetValue(input.ToLower(), out result);
+			result ??= input;
+		}
+		else
+		{
+			Abbreviations.TryGetValue(input[..^endingNumbers.Length].ToLower(), out result);
+			result = string.IsNullOrEmpty(result) ? input : result + endingNumbers;
+		}
 
-        if(variable.Contains("Number"))
-            return "int";
-        if(variable.Contains("Width"))
-            return "float";
 
-        return "string";
-    }
+		if (result.Skip(1).Any(char.IsLower) || result.Length is 1)
+		{
+			areAllCapital = false;
+		}
+		else
+		{
+			result = result.ToUpper();
+			areAllCapital = true;
+		}
 
-    private static readonly Dictionary<string, string> Abbreviations = new()
-    {
-        { "cust", "Customer" },
-        { "num", "Number" },
-        { "prod", "Product" },
-        { "equip", "Equipment" },
-        { "est", "Estimate" },
-        { "descr", "Description" },
-        { "desc", "Description" }
-    };
+		return result;
+	}
+
+	private static string GetType(string? comment, string variable)
+	{
+		if (comment != null)
+		{
+			return comment.ToLower() switch
+			{
+				"alpha" => "string",
+				"text" => "string",
+				"real" => "float",
+				"integer" => "int",
+				"long" => "long",
+				"boolean" => "bool",
+				"10" => "string", //alpha
+				"8" => "DateOnly", //date
+				"4" => "long", //long integer
+				"6" => "float", //real
+				"9" => "long", //time
+				"1" => "bool", //boolean
+				_ => "string"
+			};
+		}
+
+		if (variable.Contains("Number"))
+			return "int";
+		if (variable.Contains("Width"))
+			return "float";
+
+		return "string";
+	}
+
+	private static readonly Dictionary<string, string> Abbreviations = new()
+	{
+		{ "cust", "Customer" },
+		{ "num", "Number" },
+		{ "prod", "Product" },
+		{ "equip", "Equipment" },
+		{ "est", "Estimate" },
+		{ "descr", "Description" },
+		{ "desc", "Description" }
+	};
 }
